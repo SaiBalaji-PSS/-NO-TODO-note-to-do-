30bc9b47b50552cf55c2261e3abed413e1645923
SaiBalaji <51410810+SaiBalaji22@users.noreply.github.com>
2019-11-24 15:34:05 +0530

    Added ui for login activity

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
